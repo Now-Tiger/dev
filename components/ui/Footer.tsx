5a9c2cd1ee77ab52c5e27c67936db10c1d6abddb
Swapnil Narwade <swapnil.narwade3@gmail.com>
2025-10-28 21:05:23 +0530

    Added footer

---
+++ components/ui/Footer.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+const FootNote = () => {
+  return (
+    <>
+      <main className="flex items-center justify-center">
+        <div className="flex items-center space-x-3">
+          <span className="text-gray-500 lg:text-xs text-xs md:text-base mb-5 leading-snug font-light">©Swapnil Narwade</span>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default FootNote;
